7ae1bd5cb93e7f99dff16b6b4901376375fff853
Cristian Home <chome@codebranch.co>
2023-02-10 14:35:13 -0500

    Section 2.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,19 @@
 import Alpine from "alpinejs";
 
-window.Alpine = Alpine;
+// window.Alpine = Alpine;
+
+
+Alpine.store("menu", {
+    isOpen: false,
+    open: function () {
+        this.isOpen = true;
+    },
+    close: function () {
+        this.isOpen = false;
+    },
+    toggle: function () {
+        this.isOpen = !this.isOpen;
+    },
+});
 
 Alpine.start();
