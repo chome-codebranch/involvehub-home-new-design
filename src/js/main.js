f9fa539598ac0cab76b00b585279913fb5428c72
Cristian Home <chome@codebranch.co>
2023-02-15 08:41:05 -0500

    Open link when get demo  button is clicked.

--- src/js/main.js
+++ src/js/main.js
@@ -13,4 +13,14 @@ Alpine.store("menu", {
     },
 });
 
+Alpine.store("getDemo", {
+    getADemo: function () {
+        // open url in new tab
+        window.open(
+            "https://us12.list-manage.com/contact-form?u=cd38d9f7d87cceca871f714ec&form_id=302e7aebb7557eed993a1a6d76857960",
+            "_blank"
+        );
+    },
+});
+
 Alpine.start();
